142a23a35399aaa2fe512ee815406992899f32d6
Babyyoda777 <azlan777@icloud.com>
2022-11-20 12:00:23 +0000

    Week 1 Draft

---
+++ Publications/CSS+JS/main.js
@@ -0,0 +1,61 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbywuLlAWki6S53SICEcFAUK328pHGTzV7aJ-PLT_dUdrejOKk8w022eeG98fkuKURz1ug/exec'
+const form = document.forms['submit-to-google-sheet']
+
+
+window.addEventListener("load", function() {
+  const form = document.getElementById('my-form');
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: 'POST',
+      body: data,
+    })
+    .then(() => {
+      alert("Success!");
+    })
+  });
+});
+
+  function myFunction() {
+    var x = document.getElementById("nav-menu");
+    if (x.className === "nav__menu") {
+      x.className += " show-menu";
+    } else {
+      x.className = "nav__menu";
+    }
+  }
+
+  AOS.init({
+    offset: 50,
+    duration: 800
+  });
+
+
+
